27fe63e9193fb8187051f4488e23b1d0159be29f
Jay R Bolton <jayrbolton@gmail.com>
2016-08-17 13:10:00 -0700

    Add a validator for :array_of_hashes, which does nested validations, plus a special :root key, and clean up some code and add tests

--- lib/param_validation.rb
+++ lib/param_validation.rb
@@ -4,17 +4,18 @@ class ParamValidation
   # Given a hash of data and a validation hash, check all the validations, raising an Error on the first invalid key
   def initialize(data, validations)
     validations.each do |key, validators|
-      msg = validations[key][:message]
+      val = key === :root ? data : data[key]
       validators.each do |name, arg|
-        val = data[key]
-        validator = @@vals[name]
+        validator = @@validators[name]
+        msg = validations[key][:message]
         next unless validator
-        is_valid = @@vals[name].call(val, arg, data)
+        is_valid = @@validators[name].call(val, arg, data)
         msg_proc = @@messages[name]
         msg ||= @@messages[name].call({key: key, data: data, val: val, arg: arg}) if msg_proc
-        raise Error.new({key: key, val: val, name: name, message: msg}) unless is_valid
+        raise Error.new(msg, {key: key, val: val, name: name}) unless is_valid
       end
     end
+    return true
   end
 
   def self.messages; @@messages; end
@@ -22,12 +23,20 @@ class ParamValidation
     @@messages[name] = block
   end
 
-  def self.validators; @@vals; end
+  def self.validators; @@validators; end
   def self.add_validator(name, &block)
-    @@vals[name] = block
+    @@validators[name] = block
+  end
+  def self.structure_validators; @@structure_validators; end
+  def self.add_structure_validator(name, &block)
+    @@structure_validators[name] = block
   end
 
-  @@vals = {
+  # In each Proc
+  #  - val is the value we are actually validating from the data passed in
+  #  - arg is the argument passed into the validator (eg for {required: true}, it is `true`)
+  #  - data is the entire set of data
+  @@validators = {
     required:  Proc.new {|val, arg, data| !val.nil?},
     absent: Proc.new {|val, arg, data| val.nil?},
     not_included_in: Proc.new {|val, arg, data| !arg.include?(val)},
@@ -42,7 +51,11 @@ class ParamValidation
     equals: Proc.new{|val, arg, data| val == arg},
     min: Proc.new{|val, arg, data| val >= arg},
     max: Proc.new{|val, arg, data| val <= arg},
-    in_range: Proc.new{|val, arg, data| arg.cover?(val)}
+    is_array: Proc.new{|val, arg, data| val.is_a?(Array)},
+    is_hash: Proc.new{|val, arg, data| val.is_a?(Hash)},
+    is_json: Proc.new{|val, arg, data| ParamValidation.is_valid_json?(val)},
+    in_range: Proc.new{|val, arg, data| arg.cover?(val)},
+    array_of_hashes: Proc.new{|val, arg, data| data.is_a?(Array) && data.map{|key, val| ParamValidation.new({key: val}, arg)}.all?}
   }
 
   @@messages = {
@@ -51,7 +64,7 @@ class ParamValidation
     not_included_in: Proc.new {|h| "#{h[:key]} must not be included in #{h[:arg].join(", ")}"},
     included_in: Proc.new {|h|"#{h[:key]} must be one of #{h[:arg].join(", ")}"},
     format: Proc.new {|h|"#{h[:key]} doesn't have the right format"},
-    is_integer: Proc.new {|h|"#{h[:key]} "},
+    is_integer: Proc.new {|h|"#{h[:key]} should be an integer"},
     is_float: Proc.new {|h|"#{h[:key]} is required"},
     min_length: Proc.new {|h|"#{h[:key]} is required"},
     max_length: Proc.new {|h|"#{h[:key]} is required"},
@@ -60,28 +73,34 @@ class ParamValidation
     equals: Proc.new {|h|"#{h[:key]} is required"},
     min: Proc.new {|h|"#{h[:key]} is required"},
     max: Proc.new {|h|"#{h[:key]} is required"},
-    in_range: Proc.new {|h|"#{h[:key]} is required"}
+    in_range: Proc.new {|h|"#{h[:key]} is required"},
+    array_of_hashes: Proc.new{|h| "Please pass in an array of hashes"}
   }
 
 
   class Error < RuntimeError
     attr_accessor :key, :message, :val, :name
-    def initialize(data)
-      @message = data[:message]
-      @key = data[:key]
-      @val = data[:val]
-      @name = data[:name]
+    def initialize(message, data)
+      @data = data
+      super message
     end
-
-    def to_h
-      {message: @message, key: @key, val: @val, name: @name}
-    end
+    def data; @data; end
   end
 
   # A convient error class to return if a required record is not found
   class NotFound < RuntimeError
     def initialize(message="Record not found.")
-      super
+      super(message)
+    end
+  end
+
+  # small utility for testing json validity
+  def self.is_valid_json?(str)
+    begin
+      JSON.parse(str)
+      return true
+    rescue JSON::ParseError => e
+      return false
     end
   end
 end
--- test/param_validation_test.rb
+++ test/param_validation_test.rb
@@ -7,85 +7,95 @@ class ParamValidationTest < Minitest::Test
   def setup
   end
 
-  def test_require
+  def test_required
     begin; ParamValidation.new({}, {x: {required: true}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal :x, e.data[:key]
   end
   def test_require_no_err
     begin; ParamValidation.new({x: 1}, {x: {required: true}})
-    rescue ParamValidation::Error => e; e; end
+    rescue ParamValidation::Error => e; end
     assert e.nil?
   end
   def test_absent
     begin; ParamValidation.new({x: 1}, {x: {absent: true}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal :x, e.data[:key]
   end
   def test_not_included_in
     begin; ParamValidation.new({x: 1}, {x: {not_included_in: [1]}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal :x, e.data[:key]
   end
   def test_included_in
     begin; ParamValidation.new({x: 1}, {x: {included_in: [2]}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal :x, e.data[:key]
   end
   def test_format
     begin; ParamValidation.new({x: 'x'}, {x: {format: /y/}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal :x, e.data[:key]
   end
   def test_is_integer
     begin; ParamValidation.new({x: 'x'}, {x: {is_integer: true}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal :x, e.data[:key]
   end
   def test_is_float
     begin; ParamValidation.new({x: 'x'}, {x: {is_float: true}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal :x, e.data[:key]
   end
   def test_min_length
     begin; ParamValidation.new({x: []}, {x: {min_length: 2}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal :x, e.data[:key]
   end
   def test_max_length
     begin; ParamValidation.new({x: [1,2,3]}, {x: {max_length: 2}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal e.data[:key], :x
   end
   def test_length_range
     begin; ParamValidation.new({x: [1,2,3,4]}, {x: {length_range: 1..3}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal e.data[:key], :x
   end
   def test_length_equals
     begin; ParamValidation.new({x: [1,2]}, {x: {length_equals: 1}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal e.data[:key], :x
   end
   def test_min
     begin; ParamValidation.new({x: 1}, {x: {min: 2}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.key, :x
+    assert_equal e.data[:key], :x
   end
   def test_max
     begin; ParamValidation.new({x: 4}, {x: {max: 2}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.name, :max
+    assert_equal e.data[:name], :max
   end
   def test_in_range
     begin; ParamValidation.new({x: 1}, {x: {in_range: 2..4}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.val, 1
+    assert_equal e.data[:val], 1
   end
   def test_equals
     begin; ParamValidation.new({x: 1}, {x: {equals: 2}})
     rescue ParamValidation::Error => e; e; end
-    assert_equal e.message, "x is required"
+    assert_equal "x is required", e.to_s
+  end
+  def test_root_array_of_hashes
+    begin; ParamValidation.new({x: 1}, {root: {array_of_hashes: {x: {required: true}}}})
+    rescue ParamValidation::Error => e; e; end
+    assert_equal "Please pass in an array of hashes", e.to_s
+  end
+  def test_root_array_of_hashes_with_nesting
+    begin; ParamValidation.new([{x: 1}, {x: 'hi'}], {root: {array_of_hashes: {x: {is_integer: true}}}})
+    rescue ParamValidation::Error => e; e; end
+    assert_equal "x should be an integer", e.to_s
   end
 
   def test_add_validator
@@ -95,7 +105,7 @@ class ParamValidationTest < Minitest::Test
     rescue ParamValidation::Error => e
       e
     end
-    assert_equal e.name, :dollars
+    assert_equal :dollars, e.data[:name]
   end
   def test_set_message
     ParamValidation.add_validator(:dollars){|val, arg, data| val =~ /^\d+(\.\d\d)?$/}
@@ -105,6 +115,6 @@ class ParamValidationTest < Minitest::Test
     rescue ParamValidation::Error => e
       e
     end
-    assert_equal e.message, "x must be a dollar amount"
+    assert_equal "x must be a dollar amount", e.to_s
   end
 end
